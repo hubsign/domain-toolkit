a53cc1e7106635c816b4452645034043008164e2
Maksim <kodermax@gmail.com>
2024-03-26 21:43:40 +0300

    up

--- src/domains/index.ts
+++ src/domains/index.ts
@@ -2,10 +2,14 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 
 import { verifyHandler, verifyRoute } from "./verify";
 import { addHandler, addRoute } from "./add";
+import { getHandler, getRoute } from "./get";
+import { configHandler, configRoute } from "./config";
 
 const domainsApi = new OpenAPIHono<{}>();
 
 domainsApi.openapi(addRoute, addHandler);
+domainsApi.openapi(getRoute, getHandler);
+domainsApi.openapi(configRoute, configHandler);
 domainsApi.openapi(verifyRoute, verifyHandler);
 
 export { domainsApi };
